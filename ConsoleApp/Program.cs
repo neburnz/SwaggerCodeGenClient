e7987f30cba7679678528564a44eae0f4765265b
Rubén Guzmán <neburnz@hotmail.com>
2020-05-14 23:19:46 -0500

    Update README.md

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WebApi.Client;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // TODO: Invocar API a traves de Client
+            var client = new ApiClient();
+            
+        }
+    }
+}
